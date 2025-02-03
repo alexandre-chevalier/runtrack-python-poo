5db4d79a4c1155c629d1189003d4cd0ad898d080
alexandre <alexandre.chevalier@laplateforme.io>
2025-02-03 13:10:35 +0100

    job5 done

--- jour01/jobs05/jobs05.py
+++ jour01/jobs05/jobs05.py
@@ -1,3 +1,39 @@
-#creer classe poit avec des attributs x et y avec
+#creer classe point avec des attributs x et y avec
 # des methodes afficherPoint, afficherX, afficherY
 # changerX et changerY
+
+
+class Point:
+    def __init__(self, x, y ):
+        self.x = x
+        self.y = y
+
+    def afficherLesPoints(self):
+        print(f"voici les coordonnées ({self.x}, {self.y})")
+
+    def afficherX(self):
+        print(f"Voici la coordonné de x :  {self.x}")
+
+    def afficherY(self):
+        print(f"ceci est la coordonnées y : {self.y}")
+
+    def changerX(self):
+        X = input("veuillez entrer votre nouveaux x : ")
+        self.x = X
+
+    def changerY(self):
+        Y = input("veuillez entrer votre nouveaux x : ")
+        self.y = Y
+
+
+coordonnées = Point(10, 5)
+
+coordonnées.afficherX()
+coordonnées.afficherY()
+
+coordonnées.changerX()
+coordonnées.changerY()
+
+coordonnées.afficherX()
+coordonnées.afficherY()
+
