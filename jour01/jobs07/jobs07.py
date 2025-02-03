6c6efd46095dcd34482e1f240c41a556292ac331
alexandre <alexandre.chevalier@laplateforme.io>
2025-02-03 14:37:50 +0100

    job7 really  done, the character move and the table update the position

--- jour01/jobs07/jobs07.py
+++ jour01/jobs07/jobs07.py
@@ -1,23 +1,35 @@
 class Personnage:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.x = 2
+        self.y = 2
+        self.tab = [
+            ["-","","","","","",""],
+            ["-","","","","","",""],
+            ["-","","","","","",""],
+            ["-","","","","","",""],
+            ["-","","","","","",""]
+        ]
     
     def gauche(self):
+        self.tab[self.x][self.y] ="-"
         self.x -=1
 
     def droite(self):
+        self.tab[self.x][self.y] ="-"
         self.x +=1
 
     def haut(self):
+        self.tab[self.x][self.y] ="-"
         self.y +=1
 
     def bas(self):
+        self.tab[self.x][self.y] ="-"
         self.y -=1
 
     def position(self):
-        return (self.x, self.y)
-    
+        self.tab[self.x][self.y] ="-"
+        self.tab[self.x][self.y] ="P"
+        print(self.tab, end="")
 
 perso_dep = Personnage()
 
@@ -30,11 +42,4 @@ print(perso_dep.position())
 
 perso_dep.haut()
 perso_dep.gauche()
-
-print(perso_dep.position())
-
-perso_dep.haut()
-perso_dep.gauche()
-
-print(perso_dep.position())
 
