adf81998b214940192a50a684c988220a18d8d34
alexandre <alexandre.chevalier@laplateforme.io>
2025-02-03 10:58:51 +0100

    job4 done, i print the name of many instances

--- jour01/jobs04/jobs04.py
+++ jour01/jobs04/jobs04.py
@@ -1,1 +1,17 @@
 ## creer une classe presentation qui a des attributs nom et prenom
+
+class Presentation:
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
+
+    def sePresenter(self):
+        print(f"vous etes : {self.nom} {self.prenom}")
+
+pres1 = Presentation("chevalier", "alexandre")
+pres2 = Presentation("Ponce", "emilie")
+pres3 = Presentation("leca", "antoine")
+
+pres1.sePresenter()
+pres2.sePresenter()
+pres3.sePresenter()
